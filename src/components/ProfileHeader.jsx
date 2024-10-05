e344843f237b84aade46021f3f076a36e0916bd4
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-10-05 10:48:19 +1000

    Conditional rendering of profile image

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,18 +1,19 @@
 import { useSpotifyProfileData } from "../contexts/SpotifyProfileProvider"
 
 
-export function ProfileHeader() {
+export function ProfileHeader(){
 
-    let profileData = useSpotifyProfileData();
+	let profileData = useSpotifyProfileData();
 
-    if (profileData.id) {
-        return <div id="profileHeader">
-            <h1>Spotify Stats for {profileData.display_name}</h1>
-            <img src={profileData.images[0].url} />
-        </div>
-    } else {
-        return <div id="profileHeader">
-            <p>Please sign-in to see your Profile Data.</p>
-        </div>
-    }
+	if (profileData.id){
+		return <div id="profileHeader">
+			<h1>Spotify Stats for {profileData.display_name}</h1>
+			{profileData.images.length > 0 && <img src={profileData.images[0].url} />}
+			
+		</div>
+	} else {
+		return <div id="profileHeader">
+			<p>Please sign in to see your profile data.</p>
+		</div>
+	}
 }
